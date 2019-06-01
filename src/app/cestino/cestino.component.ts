4ff382778ea89ccdd890ed76ec23dfdd6f1e4ee4
2000AndreaPaolo <andrea.andre60@gmail.com>
2019-06-01 19:36:02 +0200

    Aggiunta del restore degli atleti

--- src/app/cestino/cestino.component.ts
+++ src/app/cestino/cestino.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
+import { CestinoService } from "../services/cestino.service";
+import { Atleti, Note, Pesi } from '../model';
 @Component({
   selector: 'app-cestino',
   templateUrl: './cestino.component.html',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CestinoComponent implements OnInit {
 
-  constructor() { }
+  atleti: Atleti[];
+  note: Note[];
+  pesi: Pesi[];
 
-  ngOnInit() {
+  constructor(
+    private cestinoService:CestinoService,
+    private toastr: ToastrService
+  ){}
+
+  ngOnInit(){
+    this.cestinoService.getAtleti_eliminati().subscribe((data: Atleti[]) => {
+      this.atleti = data;
+    });
+    this.cestinoService.loadAtleti_eliminati();
   }
 
+  restore(id_atleta: number){
+    this.cestinoService.restoreAtleta(id_atleta).subscribe((data) => {
+      if(data['code'] == 200){
+        this.cestinoService.loadAtleti_eliminati();
+        this.toastr.success('Atleta rimosso con successo', 'Successo');
+      }else{
+        this.cestinoService.loadAtleti_eliminati();
+        this.toastr.error('Atleta non rimosso', 'Errore');
+      }
+    });
+  }
 }
--- src/app/services/cestino.service.ts
+++ src/app/services/cestino.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
+import { Atleti } from '../model';
 @Injectable({
   providedIn: 'root'
 })
 export class CestinoService {
 
-  constructor() { }
+  private atleti:Subject<Atleti[]>;
+
+  constructor(private http: HttpClient){
+    this.atleti = new Subject<Atleti[]>();
+  }
+
+  getAtleti_eliminati(): Observable<Atleti[]> {
+    return this.atleti.asObservable();
+  }
+
+  loadAtleti_eliminati(): void {
+    this.http.get<Atleti[]>('/admin/deleted/atleti').subscribe(res => this.atleti.next(res));
+  }
+
+  restoreAtleta(id_atleta: number){
+    let headers = new HttpHeaders({});
+    return this.http.post("/admin/deleted/atleti/restore", JSON.stringify(id_atleta), { headers: headers });
+  }
 }
