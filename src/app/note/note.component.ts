db4ada7eec472774daeab511fa2fc485be0ca74b
2000AndreaPaolo <andrea.andre60@gmail.com>
2019-11-05 12:04:09 +0100

    Aggiunta del filtro atleta nelle note

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -123,4 +123,36 @@ export class NoteComponent implements OnInit {
       this.note = appoggio;
     });
   }
+
+  onChangeIdAtletaFilter(id_atleta:any){
+    console.log(id_atleta);
+    this.note = [];
+    this.noteService.getNota(this.authUser.id_coach).subscribe((data: Note[]) => {
+      if(id_atleta == ''){
+        return this.note = data;
+      }else{
+        let appoggio = [];
+        for(let dato of data){
+          if(dato.id_atleta == id_atleta){
+            appoggio.push(dato);
+          }
+        }
+        return this.note = appoggio;
+      }
+    });
+    /*let appoggio = [];
+    this.pesi = [];
+    this.pesoService.getPeso(this.authUser.id_coach).subscribe((data: Peso[]) => {
+      if(id_atleta == ''){
+        this.pesi = data;
+      }else{
+        for(let dato of data){
+          if(dato.id_atleta == id_atleta){
+            appoggio.push(dato);
+          }
+        }
+        this.pesi = appoggio;
+      }
+    });*/
+  }
 }
